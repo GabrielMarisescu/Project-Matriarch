eabb32706b62209564d05d3d819c15d74d8aa4a3
GabrielMarisescu <gabriel.lavoro10@gmail.com>
2023-11-07 20:40:16 +0100

    first commit, added sass

---
+++ src/app/components/CyberPunkButton/CyberPunkButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CyberPunkButton.scss";
+
+function CyberPunkButton() {
+  return <div className="cyberButton">CyberPunkButton</div>;
+}
+
+export default CyberPunkButton;
